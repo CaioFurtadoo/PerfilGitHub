d609f90fc58339f3dce602dbcfa4e2a35f4f98fe
Caio Furtado de Miranda Carvalho <164534948+CaioFurtadoo@users.noreply.github.com>
2025-04-02 10:39:41 -0300

    fixed background

--- src/components/Background/index.jsx
+++ src/components/Background/index.jsx
@@ -4,8 +4,8 @@ import ellipse2 from "../../assets/Ellipse2.svg";
 export const Background = () => {
   return (
     <>
-        <img className="z-10 w-[888px] absolute top-[-200px] right-[-100px]  animate-wiggle" src={ellipse1} alt="ellipse 1 image" />
-        <img className="z-30 w-[500px] absolute top-[40px] left-[-20px]  animate-wiggle" src={ellipse2} alt="ellipse 2 image" />
+        <img className="z-10 w-[888px] absolute top-[-200px] max-[768px]:animate-opacity right-[-100px] max-[768px]:top-0 max-[768px]:right-0   animate-wiggle" src={ellipse1} alt="ellipse 1 image" />
+        <img className="z-30 w-[500px] max-[768px]:bottom-0  max-[768px]:left-0 absolute top-[40px] left-[-20px] animate-wiggle max-[768px]:animate-opacity" src={ellipse2} alt="ellipse 2 image" />
     </>
   );
 };
